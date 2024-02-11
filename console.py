d67d52dac3a98f980936a03567fc1e2cf4d048a6
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-11 20:09:27 +0200

    first console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""entry point of the command interpreter."""
+import cmd
+import sys
+from models.base_model import BaseModel
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    do_EOF = do_exit = do_quit
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
